c41bb690fdcc0f0059616568cc5532bbd5b1ec0e
Squall Leonhart <Knight1988@gmail.com>
2022-12-06 08:45:33 +0700

    initial commit

---
+++ TwitterApp/Program.cs
@@ -0,0 +1,7 @@
+using TwitterApp;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
