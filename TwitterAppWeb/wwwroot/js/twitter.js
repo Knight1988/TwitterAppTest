22f2440f6a123571d4918fce3ae637e6d5ae9ed1
Squall Leonhart <Knight1988@gmail.com>
2022-12-11 11:18:03 +0700

    add dayjs

--- TwitterAppWeb/wwwroot/js/twitter.js
+++ TwitterAppWeb/wwwroot/js/twitter.js
@@ -1,6 +1,48 @@
-﻿var connection = new signalR.HubConnectionBuilder().withUrl("/twitterHub").build();
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
-});
+﻿$(() => {
+    const tweetCountChart = document.getElementById('tweetCountChart');
+    const connection = new signalR.HubConnectionBuilder().withUrl("/twitterHub").build();
+    connection.start().then(function () {
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    connection.on("ReceiveAnalytic", function (tweetCount, averageTweetPerMinute) {
+        document.getElementById("tweetCount").innerHTML = tweetCount;
+        document.getElementById("averageTweetPerMinute").innerHTML = averageTweetPerMinute;
+        
+        addData(tweetCountChart, {x: '2016-12-25', y: 20})
+    });
+
+    function addData(chart,  data) {
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.push(data);
+        });
+        chart.update();
+    }
+
+    function removeData(chart) {
+        chart.data.labels.pop();
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.pop();
+        });
+        chart.update();
+    }
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: 'Tweet Received',
+                data: [{x: '2016-12-25', y: 20}, {x: '2016-12-26', y: 10}]
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+})
+
