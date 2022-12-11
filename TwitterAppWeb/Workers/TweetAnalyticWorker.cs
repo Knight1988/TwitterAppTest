ad89d41b2c9ae1e9a4d76f9e10806e235f83282f
Squall Leonhart <Knight1988@gmail.com>
2022-12-11 11:20:39 +0700

    move dayjs

---
+++ TwitterAppWeb/Workers/TweetAnalyticWorker.cs
@@ -0,0 +1,21 @@
+﻿namespace TwitterAppWeb.Workers;
+
+public class TweetWorker : BackgroundService
+{
+    private readonly ILogger<TweetAnalyticBackgroundWorker> _logger;
+    private readonly MainViewModel _mainViewModel;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public TweetAnalyticBackgroundWorker(ILogger<TweetAnalyticBackgroundWorker> logger, MainViewModel mainViewModel, IServiceScopeFactory serviceScopeFactory)
+    {
+        _logger = logger;
+        _mainViewModel = mainViewModel;
+        _serviceScopeFactory = serviceScopeFactory;
+        DoWork += OnDoWork;
+    }
+    
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
